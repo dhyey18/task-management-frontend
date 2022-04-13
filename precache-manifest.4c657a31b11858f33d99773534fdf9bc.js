d6f5e098e27b697beeb07f6e4a0c24c05fa132fd
dhyey18 <77156521+dhyey18@users.noreply.github.com>
2022-04-13 16:37:15 +0530

    Updates

---
+++ precache-manifest.4c657a31b11858f33d99773534fdf9bc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a41ad925eb712e2f8b905ad02669b8d",
+    "url": "/task-management-frontend/index.html"
+  },
+  {
+    "revision": "63d9bcac6e52e58ad880",
+    "url": "/task-management-frontend/static/css/main.b9313b8c.chunk.css"
+  },
+  {
+    "revision": "bfbb88aa29b52402bb4e",
+    "url": "/task-management-frontend/static/js/2.d7f972ce.chunk.js"
+  },
+  {
+    "revision": "63d9bcac6e52e58ad880",
+    "url": "/task-management-frontend/static/js/main.925917ea.chunk.js"
+  },
+  {
+    "revision": "170cb679b8a6db4918f5",
+    "url": "/task-management-frontend/static/js/runtime~main.961a32b0.js"
+  }
+]);
